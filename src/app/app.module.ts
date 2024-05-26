ea59ef4f2dca44f0195d95200f4f0205df9256a4
Daniel <danigutierritos2817@gmail.com>
2024-05-26 09:56:29 -0500

    Descripción de tus cambios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PaginaprincipalComponent } from './paginaprincipal/paginaprincipal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaginaprincipalComponent
   ],
   imports: [
     BrowserModule,
