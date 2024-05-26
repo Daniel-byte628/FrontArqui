ca542a57e4e364dd650e270a99e1c736ade761a2
David <davids_barretom@javeriana.edu.co>
2024-05-26 17:11:31 -0500

    Addition of landing page

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -7,5 +7,30 @@ import { Session, login, handleIncomingRedirect, getDefaultSession } from '@inru
   styleUrls: ['./inicio.component.css']
 })
 export class InicioComponent {
-
+featuredProducts = [
+    {
+      name: 'Producto 1',
+      description: 'Descripción del producto 1.',
+      price: '$100',
+      image: 'assets/product1.jpg'
+    },
+    {
+      name: 'Producto 2',
+      description: 'Descripción del producto 2.',
+      price: '$200',
+      image: 'assets/product2.jpg'
+    },
+    {
+      name: 'Producto 3',
+      description: 'Descripción del producto 3.',
+      price: '$300',
+      image: 'assets/product3.jpg'
+    },
+    {
+      name: 'Producto 4',
+      description: 'Descripción del producto 4.',
+      price: '$400',
+      image: 'assets/product4.jpg'
+    }
+  ];
 }
