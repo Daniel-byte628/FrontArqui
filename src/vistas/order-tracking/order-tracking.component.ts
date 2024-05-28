810ca3fb252ad3cd680550ddc43e2e5d5de68537
Daniel <danigutierritos2817@gmail.com>
2024-05-27 22:36:49 -0500

    solid url lugar del tipo

--- src/vistas/order-tracking/order-tracking.component.ts
+++ src/vistas/order-tracking/order-tracking.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { getStringNoLocale, getUrl } from '@inrupt/solid-client';
+import { VCARD } from '@inrupt/vocab-common-rdf';
+import { SolidAuthService } from '../../controlador/service/solid-auth.service';
 
 @Component({
   selector: 'app-order-tracking',
   templateUrl: './order-tracking.component.html',
   styleUrl: './order-tracking.component.css'
 })
-export class OrderTrackingComponent {
+export class OrderTrackingComponent implements OnInit {
+  address: string = '';
+  locality: string = '';
+  postalCode: string = '';
+  region: string = '';
+  countryName: string = '';
 
+  constructor(private solidAuthService: SolidAuthService) { }
+
+  async ngOnInit(): Promise<void> {
+    const webId = localStorage.getItem('webId');
+    console.log(webId);
+    if (webId) {
+      const profile = await this.solidAuthService.fetchProfile(webId);
+
+      const addressUrl = getUrl(profile, VCARD.hasAddress);
+      if (addressUrl) {
+        const addressNode = await this.solidAuthService.fetchProfile(addressUrl);
+        if (addressNode) {
+          this.address = getStringNoLocale(addressNode, VCARD.street_address) || '';
+          this.locality = getStringNoLocale(addressNode, VCARD.locality) || '';
+          this.postalCode = getStringNoLocale(addressNode, VCARD.postal_code) || '';
+          this.region = getStringNoLocale(addressNode, VCARD.region) || '';
+          this.countryName = getStringNoLocale(addressNode, VCARD.country_name) || '';
+        } else {
+          console.error('No se pudo obtener la información de dirección del perfil.');
+        }
+      } else {
+        console.error('No se encontró la URL de la dirección en el perfil.');
+      }
+    } else {
+      console.error('No se encontró el webId en el almacenamiento local.');
+    }
+  }
 }
